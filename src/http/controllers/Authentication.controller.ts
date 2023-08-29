969de202563bca2fedf442d7d88884428cbb3706
Unknower <unknower@Unknowers-MBP.attlocal.net>
2023-08-29 00:47:20 -0400

    log bad host names

--- src/http/controllers/Authentication.controller.ts
+++ src/http/controllers/Authentication.controller.ts
@@ -20,7 +20,8 @@ router.get("/web3", async (req: Request, res: Response) => {
   const address = extractToken(req).toLowerCase(),
     clientIp = req.clientIp;
   try {
-    if (!address || process.env.NODE_ENV === "production" && req.hostname !== "vlm.gg" &&  req.hostname !== "www.vlm.gg") {
+    if (process.env.NODE_ENV === "production" && req.hostname !== "vlm.gg" && req.hostname !== "www.vlm.gg") {
+      console.log(req.hostname);
       return res.status(400).json({
         text: "Wait a minute...who ARE you?",
       });
