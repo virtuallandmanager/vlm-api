2f8984edcd9c7f1be1b561f4f0bd5735e8c51174
Unknower <dev@unknower.com>
2023-09-15 23:44:24 -0400

    fix: enable load test

--- test/ToDo_test.ts
+++ test/ToDo_test.ts
@@ -1,31 +1,31 @@
-// import assert from "assert";
-// import { ColyseusTestServer, boot } from "@colyseus/testing";
+import assert from "assert";
+import { ColyseusTestServer, boot } from "@colyseus/testing";
 
-// // import your "arena.config.ts" file here.
-// import appConfig from "../src/arena.config";
-// import { MyRoomState } from "../src/rooms/schema/MyRoomState";
+// import your "arena.config.ts" file here.
+import appConfig from "../src/arena.config";
+import { VLMSceneState } from "../src/ws/rooms/schema/VLMSceneState";
 
-// describe("testing your Colyseus app", () => {
-//   let colyseus: ColyseusTestServer;
+describe("testing your Colyseus app", () => {
+  let colyseus: ColyseusTestServer;
 
-//   before(async () => colyseus = await boot(appConfig));
-//   after(async () => colyseus.shutdown());
+  before(async () => colyseus = await boot(appConfig));
+  after(async () => colyseus.shutdown());
 
-//   beforeEach(async () => await colyseus.cleanup());
+  beforeEach(async () => await colyseus.cleanup());
 
-//   it("connecting into a room", async () => {
-//     // `room` is the server-side Room instance reference.
-//     const room = await colyseus.createRoom<MyRoomState>("my_room", {});
+  it("connecting into a room", async () => {
+    // `room` is the server-side Room instance reference.
+    const room = await colyseus.createRoom<VLMSceneState>("vlm_scene", {});
 
-//     // `client1` is the client-side `Room` instance reference (same as JavaScript SDK)
-//     const client1 = await colyseus.connectTo(room);
+    // `client1` is the client-side `Room` instance reference (same as JavaScript SDK)
+    const client1 = await colyseus.connectTo(room);
 
-//     // make your assertions
-//     assert.strictEqual(client1.sessionId, room.clients[0].sessionId);
+    // make your assertions
+    assert.strictEqual(client1.sessionId, room.clients[0].sessionId);
 
-//     // wait for state sync
-//     await room.waitForNextPatch();
+    // wait for state sync
+    await room.waitForNextPatch();
 
-//     assert.deepStrictEqual({ mySynchronizedProperty: "Hello world" }, client1.state.toJSON());
-//   });
-// });
+    assert.deepStrictEqual({ mySynchronizedProperty: "Hello world" }, client1.state.toJSON());
+  });
+});
